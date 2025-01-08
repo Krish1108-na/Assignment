eab22c7d9f420b00188368e889915e72a20637df
Krishna Singh <singhkrishna93244@gmail.com>
2025-01-08 20:33:25 +0530

    New Changes

---
+++ WorkingOfJs.js
@@ -0,0 +1,40 @@
+
+// console.log(addNumbers(15, 80)); 
+
+// function addNumbers (num1, num2) { 
+//     return num1 + num2;
+// }
+
+
+// console.log(multiplyNumbers(13, 4)); 
+// const multiplyNumbers = function (num1, num2) 
+// { 
+//     return num1 * num2;
+// };
+
+
+// function addNumbers (num1, num2) 
+// { 
+//     console.log(sum); 
+//     var sum;
+//     sum = num1 + num2;
+//     return sum;
+//     }
+//     console.log(addNumbers (15, 85)); 
+
+
+// {
+//     console.log(x); 
+//     console.log(y); 
+//     }
+//     console.log(z); 
+//     var x = "PW";
+//     let y = "Skills";
+//     const z = "!";
+//     console.log(x); 
+//     console.log(y); 
+
+
+ console.log(x); 
+let x = "hello";
+console.log(x);
